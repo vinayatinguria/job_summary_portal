915561eb7c1e0e2cb1da2d24e2aecd37315ce971
vinayatinguria <vinaya.t20@gmail.com>
2016-11-22 02:06:00 -0500

    Count common words in 2 strings

--- data_preprocessing_chord.py
+++ data_preprocessing_chord.py
@@ -1,4 +1,5 @@
-#import csv
+#import data
+import csv
 with open('data_2015.csv','rb') as f:
     reader=csv.reader(f)
     list = map(tuple, reader)
@@ -9,3 +10,15 @@ print list[1]
 data_filtered = [[l[i] for i in list2] for l in list]
 print data_filtered[1]
 
+#count common words in 2 strings
+s="Public Relation and Fundraising Managers"
+s2 = "Industrial Product Managers"
+length = len(s.split())
+count = 0
+for i in range(length):
+    word = s.split(" ", length)[i]
+    if word in s2:
+        count= count+ 1
+    else:
+        count=count+0
+print count

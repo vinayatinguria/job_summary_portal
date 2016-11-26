9b088194ec8cc0013e1d006ebe3febd3e4418dd3
vinayatinguria <vinaya.t20@gmail.com>
2016-11-26 00:25:42 -0500

    Optimized, Added code for creating matrix

--- data_preprocessing_chord.py
+++ data_preprocessing_chord.py
@@ -5,32 +5,37 @@ with open('data_2015.csv','rb') as f:
     list = map(tuple, reader)
 
 #filter job code, job title, and job group
-list2 = [3,4,5]
+list2 = [3, 4, 5]
 print list[1]
 data_filtered = [[l[i] for i in list2] for l in list]
-print data_filtered[1]
+#TODO: filter detailed jobs
+data_filtered=data_filtered[:5]
 
-#remove stopwords
-job = "Tax Examiners and Collectors, and Revenue Agents"
+job_title=[]
+for data in data_filtered:
+    job_title.append(data[1])
+
+#remove stopwords and commas
 stopwords = ['and','Other','All','Including','Except']
-splitwords = job.split()
-
-resultwords = [word for word in splitwords if word not in stopwords]
-result = ' '.join(resultwords)
-print result
-
-#remove comma
-result_nocomma = result.replace(",","")
-print result_nocomma
+cleaned = []
+for job in job_title:
+    splitwords=job.split()
+    cleanedwords = [word for word in splitwords if word not in stopwords]
+    cleanedwords = ' '.join(cleanedwords)
+    cleanedwords = cleanedwords.replace(",","")
+    cleaned.append(cleanedwords)
+print cleaned
 
 #count common words in 2 strings
-s2 = "Public Relation Fundraising Managers"
-length = len(result_nocomma.split())
-count = 0
-for i in range(length):
-    word = result_nocomma.split(" ", length)[i]
-    if word in s2:
-        count= count+ 1
-    else:
-        count=count+0
-print count
+w= len(cleaned)
+matrix = [[0 for x in range(w)] for y in range(w)]
+for i in range(len(cleaned)):
+    for j in range(len(cleaned)):
+        words = cleaned[i].split()
+        count=0
+        if i != j:
+            for word in words:
+                if word in cleaned[j]:
+                    count=count+1
+        matrix[i][j]=count
+print matrix
